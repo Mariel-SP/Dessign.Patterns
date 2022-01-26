8fa78f5dc74b00ab65cd5bcb880f9a194c2207cb
Mariel Salcedo P <81381529+Mariel-SP@users.noreply.github.com>
2022-01-26 11:43:11 -0500

    Create Main.py

---
+++ P.Factory/Main.py
@@ -0,0 +1,14 @@
+from Restaurante import Restaurante_Chino,Restaurante_Americano, Restaurante_Universal
+
+restaurante_a = Restaurante_Chino()
+restaurante_b = Restaurante_Americano()
+restaurante_c = Restaurante_Universal()
+
+Platillo=restaurante_a.crearPlatillo()
+Platillo.Plato();
+
+Platillo=restaurante_b.crearPlatillo()
+Platillo.Plato();
+
+Platillo=restaurante_c.crearPlatillo()
+Platillo.Plato();
