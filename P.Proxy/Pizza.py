2e59efe725475ee00d65b6c8e791783fd898204b
Mariel Salcedo P <81381529+Mariel-SP@users.noreply.github.com>
2022-02-15 12:05:05 -0500

    Create Pizza.py

---
+++ P.Proxy/Pizza.py
@@ -0,0 +1,14 @@
+
+
+class pizza:
+
+    def __init__(self,pizza_uno='pizza'):
+        self.Nuevo = pizza_uno
+
+
+    def setNueva_Pizza(self,new):
+        self.Nuevo = new
+    
+
+    def Nueva_pizza(self):
+        return self.Nuevo
